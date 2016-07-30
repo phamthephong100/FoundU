9d5a09fb57192b119be98f43d7f137300b87174e
DUONGTHOAI <jupiter.at@gmail.com>
2016-07-30 13:24:26 +0700

    add file

--- app/src/main/java/com/example/foundu/MapsActivity.java
+++ app/src/main/java/com/example/foundu/MapsActivity.java
@@ -47,7 +47,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 //        registerUser();
-        getFriendLocation();
+//        getFriendLocation();
     }
 
 
